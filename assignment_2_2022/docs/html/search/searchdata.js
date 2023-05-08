5da36c12b03471301025c423d07d939f9662784e
Andrea Bolla <107572039+boez98@users.noreply.github.com>
2023-05-08 19:49:15 +0200

    Add files via upload

---
+++ assignment_2_2022/docs/html/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "abcdfgklmnprstuwxyz",
+  1: "abdgnw",
+  2: "abdgnrw",
+  3: "cdfgmnptuw",
+  4: "adklnprsuxyz",
+  5: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Pages"
+};
+
